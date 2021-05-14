2f33ddc730b814cf3314560398182ad8828a6fea
vmayv <vasyamayorov@yandex.ru>
2021-05-14 15:30:07 +0300

    Выполнено дз 1

---
+++ WebApplication/HomeWork1/PostHandler.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class PostHandler
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public async Task<Post> GetPostAsync(string uri, int id)
+        {
+            var response = await _httpClient.GetAsync($"{uri}{id}");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException();
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Post>(content);
+
+            return result;
+        }
+    }
+}
--- WebApplication/HomeWork1/Program.cs
+++ WebApplication/HomeWork1/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using HomeWork1;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Linq;
 
 namespace WebApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var tasks = postsIds.Select(postsIds => postHandler.GetPostAsync(uri, postsIds));
+
+            var postsIds = new List<int>() { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            var uri = "https://jsonplaceholder.typicode.com/posts/";
+            var fileName = "result.txt";
+            var postHandler = new PostHandler();
+            var fileHadler = new FileHandler();
+            List<Post> posts = new List<Post>();
+
+            
+
+            foreach (var postId in postsIds)
+            {
+                try
+                {
+                    var post = await postHandler.GetPostAsync(uri, postId);
+                    posts.Add(post);
+                }
+                
+                catch (HttpRequestException exception)
+                {
+                    Console.WriteLine($"Ошибка: {exception.Message}");
+                    Console.WriteLine($"{exception.InnerException}");
+                }
+            }
+
+            fileHadler.WriteToFileAndSave(fileName, posts);
+            Console.ReadKey();
         }
     }
 }
